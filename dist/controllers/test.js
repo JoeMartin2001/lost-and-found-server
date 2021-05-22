4131a93d65fbe6a7f8a70dd7553316ca8aeb7e56
Sardorbek <sardorbekaminjonov2001@gmail.com>
2021-05-23 02:07:19 +0500

    fixed config

--- dist/controllers/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.renderTest = void 0;
-const renderTest = (req, res) => {
-    res.send("Home");
-};
-exports.renderTest = renderTest;
